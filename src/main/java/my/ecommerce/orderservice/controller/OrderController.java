438ffa4489384b2c2097dedf3781c2462b7a12dd
ewhdtls0 <ewhdtls0@naver.com>
2024-07-02 02:17:11 +0900

    kafka on multi service

--- src/main/java/my/ecommerce/orderservice/controller/OrderController.java
+++ src/main/java/my/ecommerce/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import my.ecommerce.orderservice.dto.OrderDto;
 import my.ecommerce.orderservice.jpa.OrderEntity;
 import my.ecommerce.orderservice.messagequeue.KafkaProducer;
+import my.ecommerce.orderservice.messagequeue.OrderProducer;
 import my.ecommerce.orderservice.service.OrderService;
 import my.ecommerce.orderservice.vo.RequestOrder;
 import my.ecommerce.orderservice.vo.ResponseOrder;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/order-service")
@@ -24,6 +26,7 @@ public class OrderController {
     private final Environment env;
     private final OrderService orderService;
     private final KafkaProducer kafkaProducer;
+    private final OrderProducer orderProducer;
 
     @GetMapping("/health_check")
     public String status() {
@@ -36,11 +39,16 @@ public class OrderController {
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
-        OrderDto createdOrder = orderService.createOrder(orderDto);
+//        OrderDto createdOrder = orderService.createOrder(orderDto);
+//        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
 
-        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
 
         kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
+
+        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
---
+++ src/main/java/my/ecommerce/orderservice/dto/Field.java
@@ -0,0 +1,12 @@
+package my.ecommerce.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Field {
+    private String type;
+    private boolean optional;
+    private String field;
+}
